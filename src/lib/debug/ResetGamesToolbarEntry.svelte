cfa50cf74402672a00f4f16a680caf42fb912ce0
Isha Dijcks <ishadijcks@gmail.com>
2026-06-23 16:59:22 +0200

    Add Spinamo and Centripetal!

--- src/lib/debug/ResetGamesToolbarEntry.svelte
+++ src/lib/debug/ResetGamesToolbarEntry.svelte
@@ -3,3 +3,10 @@
 </script>
 
 <button class="d-btn d-btn-danger" onclick={() => engine.features.gameManager.resetGames()}>Reset games</button>
+<button
+  class="d-btn d-btn-danger"
+  onclick={() =>
+    engine.features.gameManager.games.forEach((game) => {
+      localStorage.setItem(game.saveKey, new Date().toJSON());
+    })}>Unlock all</button
+>
