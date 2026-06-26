238f63ddea5a00260fdc48dcaeba79db8ab777c6
Isha Dijcks <ishadijcks@gmail.com>
2026-06-26 17:52:33 +0200

    Cleanup toolbar

--- src/lib/debug/ManualScanToolbarEntry.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { engine } from '$lib/game/game.svelte';
-</script>
-
-<button class="d-btn" onclick={() => engine.features.gameManager.scanForCompletions()}>Scan</button>
-
-<button class="d-btn btn-primary" onclick={() => localStorage.setItem('123ishatest/demo', new Date().toJSON())}
-  >Complete demo</button
->
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,12 @@
 
   import background from '$lib/assets/background.png';
   import ResetSaveToolbarEntry from '$lib/debug/ResetSaveToolbarEntry.svelte';
-  import ManualScanToolbarEntry from '$lib/debug/ManualScanToolbarEntry.svelte';
   import ResetGamesToolbarEntry from '$lib/debug/ResetGamesToolbarEntry.svelte';
 
   let { children } = $props();
 
   const config = new LuiConfigBuilder()
     .addToolbarItem('meta/reset-save', ResetSaveToolbarEntry)
-    .addToolbarItem('meta/manual-scan', ManualScanToolbarEntry)
     .addToolbarItem('meta/reset-game-progress', ResetGamesToolbarEntry)
 
     .build();
